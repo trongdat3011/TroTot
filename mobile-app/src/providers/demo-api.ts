c63a8ced729eace214a94ec0f44048c370e4b549
Thai Tue <dangthaitue@gmail.com>
2017-04-17 21:56:09 +0700

    get houses data from server

--- mobile-app/src/providers/demo-api.ts
+++ mobile-app/src/providers/demo-api.ts
@@ -11,23 +11,34 @@ export class DemoAPI {
     public http: Http) {}
   
   getHousesData(): Observable<any[]> {
-    let headers = new Headers();
-    headers.set('X-Airbnb-OAuth-Token', 'ik46pahd49xhxpmia1kze5bk');
+    //let headers = new Headers({'X-Airbnb-OAuth-Token': 'ik46pahd49xhxpmia1kze5bk'});
     let options = new RequestOptions();
-    options.headers = headers;
     let params = new URLSearchParams();
     params.set('client_id', '3092nxybyb0otqw18e8nh5nty');
     params.set('locale', 'en-US');
     params.set('currency', 'USD');
     options.search = params;
     return this.http.get(this.baseUrl, options)
-                    .map(this.extractData);
+                    .map(this.extractData)
+                    .catch(this.handleError)
+                    
   }
 
   private extractData(res: Response) {
     let body = res.json();
-    console.log(body);
-    return body.data || { };
+    //console.log(body);
+    return body.search_results || { };
   }
 
+  private handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    return Observable.throw(errMsg);
+  }
 }
