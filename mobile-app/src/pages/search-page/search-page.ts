77b1ba6c5235e0fa9b1aace0fb9c43e703c066f5
vietdoan <vietdoanhp1996@gmail.com>
2017-04-27 02:36:59 +0700

    add google map to search page

--- mobile-app/src/pages/search-page/search-page.ts
+++ mobile-app/src/pages/search-page/search-page.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
+declare var google;
 @Component({
   selector: 'page-search-page',
   templateUrl: 'search-page.html',
 })
 export class SearchPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public geolocation: Geolocation) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+    this.loadMap();
   }
 
+  loadMap(){
+ 
+    this.geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+    }, (err) => {
+      console.log(err);
+    });
+
+  }
 }
