a0a52da710466d21c08002b53094990c891b86ba
trongdat3011 <trantrongdat1@gmail.com>
2017-04-27 13:53:43 +0700

    House related api

--- server/model/house/schema.js
+++ server/model/house/schema.js
@@ -3,19 +3,19 @@ const Schema = mongoose.Schema;
 
 
 const houseSchema = new Schema({
-  city: { type: String },
-  lat: { type: Number, min: 0, max: 180 },
-  lng: { type: Number, min: 0, max: 180 },
-  name: { type: String },
-  person_capacity: { type: Number, min: 1 },
-  description: { type: String },
-  price: {  type: Number, min: 0 },
-  phone_number: { type: String },
+  city: { type: String, require: true },
+  lat: { type: Number, min: 0, max: 180, require: true },
+  lng: { type: Number, min: 0, max: 180, require: true },
+  name: { type: String, require: true },
+  person_capacity: { type: Number, min: 1, require: true },
+  description: { type: String, require: true },
+  price: {  type: Number, min: 0, require: true },
+  phone_number: { type: String, require: true },
   picture_url: { type: String },
   primary_host: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  public_address: { type: String },
-  reviews_count: { type: Number },
-  star_rating: { type: Number, min: 0, max: 5 },
+  public_address: { type: String, require: true },
+  reviews_count: { type: Number, default: 0 },
+  star_rating: { type: Number, min: 0, max: 5, default: 0 },
   pictures_url: [String]
 });
 
--- server/routes.js
+++ server/routes.js
@@ -4,14 +4,21 @@ const secret = require('./config').secret;
 const tokenChecker = require('./lib/tokenChecker');
 
 const User = require('./model/user/schema');
-const UserController = require('./model/user/controller');
-
 const Review = require('./model/review/schema');
+const House = require('./model/house/schema');
 
 const user = require('./model/user/router');
 const house = require('./model/house/router');
 const review = require('./model/review/router');
 
+function distance(lat1, lon1, lat2, lon2) {
+  // console.log(lat1 + " " + lon1 + " " + lat2 + " " + lon2);
+  const p = 0.017453292519943295;    // Math.PI / 180
+  const c = Math.cos;
+  const a = 0.5 - c((lat2 - lat1) * p) / 2 + c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
 
 // Api router - basic CRUD fortesting
 const apiRouter = new Router();
@@ -19,11 +26,16 @@ apiRouter.use('/house', house);
 apiRouter.use('/review', review);
 apiRouter.use('/user', user);
 
-
 const mainRouter = new Router();
 mainRouter
   // .use('/api', apiRouter)
-  .post('/register', (...args) => UserController.create(...args))
+  .post('/register', (req, res, next) => {
+    const user = new User(req.body);
+    user.save( (err) => {
+      if (err) return res.send(err);
+      res.json({ message: 'Successful!' });
+    });
+  })
   .post('/login', (req, res, next) => {
     User.findOne(req.body).select('_id username').exec( (err, user) => {
       if (err) return res.send(err);
@@ -79,6 +91,44 @@ mainRouter
       if (err) return res.send(err);
       res.json({ message: 'Successful!' });
     });
+  })
+  .post('/api/house', tokenChecker, (req, res, next) => {
+    const newHouse = Object.assign({}, req.body);
+    newHouse.primary_host = req.decoded._id;
+    const house = new House(newHouse);
+    house.save( (err) => {
+      if (err) return res.send(err);
+      res.json({ message: 'Successful~' });
+    });
+  })
+  .get('/api/house/:houseid', (req, res, next) => {
+    House.findById(req.params.houseid)
+      .populate('primary_host', 'username real_name has_profile_pic picture_url')
+      .exec( (err, house) => {
+        if (err) return res.send(err);
+        res.send(house);
+      });
+  })
+  .get('/api/house', (req, res, next) => {
+    House.find({})
+      .populate('primary_host', 'username real_name has_profile_pic picture_url')
+      .exec( (err, houses) => {
+        if (err) return res.send(err);
+        let matchedHouses = [];
+        if (req.params.radius){
+          houses.forEach( (house) => {
+            if ( distance(Number(req.query.lat), Number(req.query.lng), house.lat, house.lng) < Number(req.query.radius) ) 
+              matchedHouses.push(house);
+          });
+        } else {
+          matchedHouses = houses.map( (house) => {
+            const temp = house.toObject();
+            temp.distance = distance(Number(req.query.lat), Number(req.query.lng), house.lat, house.lng);
+            return temp;
+          });
+        }
+        res.send(matchedHouses);
+      });
   });
 
 
