02fdd5aaea49c43dff9650e1ab9211093275b89e
vietdoan <vietdoanhp1996@gmail.com>
2017-05-02 17:23:49 +0700

    add login setting

--- mobile-app/src/pages/login/login.ts
+++ mobile-app/src/pages/login/login.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Login page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
--- mobile-app/src/providers/provide-storage.ts
+++ mobile-app/src/providers/provide-storage.ts
@@ -1,38 +1,63 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
-
+import { Events } from 'ionic-angular';
 @Injectable()
 export class ProvideStorage {
-    constructor(public storage: Storage) { }
+    HAS_LOGGED_IN = 'hasLoggedIn';
+    HAS_SEEN_TUTORIAL = 'hasSeenTutorial';
+    TOKEN = 'accessToken'
+    constructor(
+        public storage: Storage,
+        public events: Events) { }
 
-    favoriteHouse(house) {
-        return new Promise(resolve => {
-            this.storage.set(house.listing.id.toString(), JSON.stringify(house));
-        })
-
+    favoriteHouse(house): void {
+        this.storage.set(house.listing.id.toString(), JSON.stringify(house));
     }
 
-    unfavoriteHouse(houseId) {
-        return new Promise(resolve => {
-            this.storage.remove(houseId.toString());
-        })
+    unfavoriteHouse(houseId): void {
+        this.storage.remove(houseId.toString());
     }
 
-    isFavoriteHouse(houseId) {
-        return new Promise<boolean>(resolve => {
-            this.storage.get(houseId.toString()).then((val) => resolve(val ? true : false));
-        })
+    isFavoriteHouse(houseId): Promise<boolean> {
+        return this.storage.get(houseId.toString()).then((val) => val ? true : false);
     }
 
     getAllFavorites() {
         return new Promise<any[]>(resolve => {
             let results = [];
-            this.storage.forEach(data => {
+            this.storage.forEach((data, key, index) => {
                 results.push(JSON.parse(data));
             });
             resolve(results);
-        });
+        }); 
     }
 
+    hasLoggedIn(): Promise<boolean> {
+        return this.storage.get(this.HAS_LOGGED_IN).then(value => value === true);
+    }
+
+    login(token): void {
+        this.storage.set(this.HAS_LOGGED_IN, true);
+        this.storage.set(this.TOKEN, token);
+        this.events.publish('user:login');
+    }
+
+    logout(): void {
+        this.storage.remove(this.HAS_LOGGED_IN);
+        this.storage.remove(this.TOKEN);
+        this.events.publish('user:logout');
+    }
+
+    getToken(): Promise<string> {
+        return this.storage.get(this.TOKEN).then(token => token);
+    }
+
+    checkHasSeenTutorial(): Promise<boolean> {
+        return this.storage.get(this.HAS_SEEN_TUTORIAL).then(value => value === true);
+    }
+
+    seenTutorial(): void {
+        this.storage.set(this.HAS_SEEN_TUTORIAL, true);
+    }
 
 }
