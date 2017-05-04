6479de0df2b99836b8893dac9405e247bf14bdf4
trongdat3011 <trantrongdat1@gmail.com>
2017-05-04 20:43:30 +0700

    Add default user profile pic

--- server/model/user/schema.js
+++ server/model/user/schema.js
@@ -6,8 +6,8 @@ const userSchema = new Schema({
   username: { type: String, required: true, index: true, unique: true },
   password: { type: String, required: true },
   real_name: { type: String, required: true },
-  has_profile_pic: { type: Boolean, default: false },
-  picture_url: { type: String, default: '' },
+  has_profile_pic: { type: Boolean, default: true },
+  picture_url: { type: String, default: 'https://a0.muscache.com/defaults/user_pic-225x225.png?v=3' },
   email: { type: String, required: true },
   phone: { type: String, required: true }
 });
