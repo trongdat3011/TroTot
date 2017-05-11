2182b07f97a89b7001e425f6a49219f6f188bdd4
trongdat3011 <trantrongdat1@gmail.com>
2017-05-11 16:31:55 +0700

    production server

--- server/config.js
+++ server/config.js
@@ -4,10 +4,13 @@ const config = {
     port: process.env.PORT || 8080
   },
   mongo: {
-    url: process.env.MONGO_DB_URI || 'mongodb://localhost/tro-tot'
+    testUrl: process.env.MONGO_DB_URI || 'mongodb://localhost/tro-tot',
+    productionUrl: process.env.MONGO_DB_URI || 'mongodb://trongdat3011:wipe1996@ds137281.mlab.com:37281/trotot-api-server',
+    test: false
   },
   secret: 'vietdoangotit',
   admin: 'trongdat3011'
 };
-
+config.mongo.url = config.mongo.test ? config.mongo.testUrl : config.mongo.productionUrl;
+console.log(config.mongo.url);
 module.exports = config;
