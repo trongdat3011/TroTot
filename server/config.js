e44994e804ea4f3100047a46fb542885ea5d42a6
Dat Tran <trantrongdat1@gmail.com>
2017-05-11 19:10:15 +0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -4,13 +4,13 @@ const config = {
     port: process.env.PORT || 8080
   },
   mongo: {
-    testUrl: process.env.MONGO_DB_URI || 'mongodb://localhost/tro-tot',
-    productionUrl: process.env.MONGO_DB_URI || 'mongodb://trongdat3011:wipe1996@ds137281.mlab.com:37281/trotot-api-server',
+    testUrl: 'mongodb://localhost/tro-tot',
+    productionUrl: 'mongodb://trongdat3011:wipe1996@ds137281.mlab.com:37281/trotot-api-server',
     test: false
   },
   secret: 'vietdoangotit',
   admin: 'trongdat3011'
 };
-config.mongo.url = config.mongo.test ? config.mongo.testUrl : config.mongo.productionUrl;
+config.mongo.url = process.env.MONGO_DB_URI || (config.mongo.test ? config.mongo.testUrl : config.mongo.productionUrl);
 console.log(config.mongo.url);
 module.exports = config;
